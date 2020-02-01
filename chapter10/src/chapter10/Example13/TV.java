c6053029e9dd91ff2648c90ed5549c9446d4cb7b
HwangZhow <54389469+HwangZhow@users.noreply.github.com>
2020-02-01 23:37:15 +0800

    2.01

---
+++ chapter10/src/chapter10/Example13/TV.java
@@ -0,0 +1,18 @@
+package chapter10.Example13;
+import java.io.*;
+public class TV {
+	String name;
+	int price;
+	public void setName(String s){
+		name=s;
+	}
+	public void setPrice(int n){
+		price=n;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getPrice(){
+		return price;
+	}
+}
