066ad786c52c0d51fba980fe5e2dbfbc151e2e10
HwangZhow <54389469+HwangZhow@users.noreply.github.com>
2020-02-03 23:20:09 +0800

    2.03

---
+++ chapter10/src/chapter10/Example15/Example10_15.java
@@ -0,0 +1,27 @@
+package chapter10.Example15;
+import java.io.*;
+import java.util.*;
+public class Example10_15 {
+	public static void main(String[] args) {
+		File file=new File("cost.txt");
+		Scanner sc=null;
+		int sum=0;
+		try{
+			sc=new Scanner(file);
+			while(sc.hasNext()){
+				try{
+					int price=sc.nextInt();
+					sum=sum+price;
+					System.out.println(price);
+				}
+				catch(InputMismatchException exo){
+					String t=sc.next();
+				}
+			}
+			System.out.println("Total Cost:"+sum+"dollar");
+		}
+		catch(Exception exp){
+			System.out.println("exp");
+		}
+	}
+}
