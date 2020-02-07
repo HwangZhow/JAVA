1938d84f5ad7917fd9e3b6fa508dfdd3fdf650a7
HwangZhow <54389469+HwangZhow@users.noreply.github.com>
2020-02-07 23:54:13 +0800

    2.07

---
+++ chapter10/src/chapter10/Example19/Example10_19.java
@@ -0,0 +1,10 @@
+package chapter10.Example19;
+import java.io.*;
+public class Example10_19 {
+	public static void main(String[] args) {
+		File file=new File("Example10_19.java");
+		WindowFileLock win=new WindowFileLock(file);
+		win.setTitle("使用文件锁");
+	}
+
+}
